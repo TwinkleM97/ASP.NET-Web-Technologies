971e3c46041ac4815989344603d398737581d7ea
Twinkle Mishra <119685153+TwinkleM97@users.noreply.github.com>
2025-12-03 10:56:44 -0500

    Add files via upload

---
+++ PracticeAssignment5/AccountManager.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using TransactionRecordApp.Models;
+using System.Threading.Tasks;
+
+namespace TransactionRecordApp.Controllers
+    {
+        public class AccountController : Controller
+        {
+            private readonly SignInManager<User> _signInManager;
+            private readonly UserManager<User> _userManager;
+
+            public AccountController(SignInManager<User> signInManager, UserManager<User> userManager)
+            {
+                _signInManager = signInManager;
+                _userManager = userManager;
+            }
+
+            [HttpGet]
+            public IActionResult Register()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Register(RegisterViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = new User { UserName = model.Username };
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        await _signInManager.SignInAsync(user, false);
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+                return View(model);
+            }
+
+            [HttpGet]
+            public IActionResult LogIn(string returnUrl = "")
+            {
+                return View(new LoginViewModel { ReturnUrl = returnUrl });
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> LogIn(LoginViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+
+                    if (result.Succeeded)
+                    {
+                        if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                            return Redirect(model.ReturnUrl);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                ModelState.AddModelError("", "Invalid username/password.");
+                return View(model);
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> LogOut()
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            [HttpGet]
+            public IActionResult AccessDenied()
+            {
+                return View();
+            }
+        }
+    }
---
+++ PracticeAssignment5/TransactionController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using TransactionRecordApp.Models;
+
+namespace TransactionRecordApp.Controllers
+{
+    /// <summary>
+    /// Controls the Add, Edit and Delete functions
+    /// </summary>
+    public class TransactionController : Controller
+    {
+        private readonly TransactionContext _transactionContext;
+        public TransactionController (TransactionContext transactionConatext)
+        {
+            _tranactionContext = transactionConatext;
+        }
+
+        /// <summary>
+        /// Action to allow the user to add a new transaction
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.TransactionTypes = _tranactionContext.TransactionType.OrderBy(t => t.Name).ToList();
+            return View("Edit", new Transaction());
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Add(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _tranactionContext.Transactions.Add(transaction);
+                _tranactionContext.SaveChanges();
+                return RedirectToAction("Index", "Transactions");
+            }
+            else
+            {
+                // Repopulate ViewBag in case of an error
+                ViewBag.Action = "Add";
+                ViewBag.TransactionTypes = _tranactionContext.TransactionType.OrderBy(t => t.Name).ToList();
+                return View("Edit", transaction);
+            }
+        }
+
+
+        /// <summary>
+        /// Allows user to edit a movie
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // Set the action to Edit
+            ViewBag.Action = "Edit";
+
+            var transaction = _tranactionContext.Transactions.Find(id);
+
+            // Get the transaction types and add them to the Viewbag
+            ViewBag.TransactionTypes = _tranactionContext.TransactionType.OrderBy(t => t.Name).ToList();
+
+            // Return the Edit form
+            return View(transaction);
+        }
+        /// <summary>
+        /// If there are no errors in the inputs, the transaction is updated
+        /// If there are errors, the page is returned with the errors
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpPost]
+        public IActionResult Edit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                // It's valid so we update the transaction
+                _tranactionContext.Transactions.Update(transaction);
+                _tranactionContext.SaveChanges();
+                return RedirectToAction("Index", "Transactions");
+            }
+            else
+            {
+                // It's not valid so we return the Transaction
+                // to the Edit view by setting the action to Edit again
+                ViewBag.Action = "Edit";
+                ViewBag.TransactionTypes = _tranactionContext.TransactionType.OrderBy(t => t.Name).ToList();
+                return View(transaction);
+            }
+        }
+
+        /// <summary>
+        /// Allows user to delete a movie
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            // Find the trnsaction in the DB and save it to a variable
+            var transaction = _tranactionContext.Transactions.Find(id);
+
+            // Return the Delete form
+            return View(transaction);
+        }
+        /// <summary>
+        /// Delete the movie
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Delete(Transaction transaction)
+        {
+            // Delete the movie
+            _tranactionContext.Transactions.Remove(transaction);
+            _tranactionContext.SaveChanges();
+            return RedirectToAction("Index", "Transactions");
+        }
+
+        private TransactionContext _tranactionContext;
+    }
+}
